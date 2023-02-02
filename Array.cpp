5c936364a5570b0c43619987e6f5f133c2a2703c
jfWarouw <120134802+jfWarouw@users.noreply.github.com>
2023-02-03 00:07:26 +0800

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string siswa[5] = { "Eko Ari", "Efi Ero", "Jorgensio", "Gustiawan", "Aby"};
+    for (int a = 0 ; a < 5; a++){
+    cout << siswa[a] << "\n";
+
+    }
+    
+  }
